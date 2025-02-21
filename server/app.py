6a5f86818f688c15cdb207b771b2e178d370d2ee
nikitaamani <nikita.bosire@student.moringaschool.com>
2025-02-21 13:55:57 +0300

    set up Flask application with SQLAlchemy and configuration; added API blueprint registration

--- server/app.py
+++ server/app.py
@@ -1,14 +1,19 @@
+# app.py
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from models import db
+from routes import api_blueprint
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-@app.route('/')
-def index():
-    return '<h1>Welcome to my page!</h1>'
+db.init_app(app)
 
-@app.route('/<string:username>')
-def user(username):
-    return f'<h1>Profile for {username}</h1>'
+# Register blueprints
+app.register_blueprint(api_blueprint, url_prefix='/api')
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    with app.app_context():
+        db.create_all()  # Ensures tables are created
+    app.run(debug=True)
