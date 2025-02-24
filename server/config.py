ace64f44b49b1469151455d1e26aa146020d171a
nikitaamani <nikita.bosire@student.moringaschool.com>
2025-02-24 11:16:42 +0300

    fix formatting of DATABASE_URL in configuration

--- server/config.py
+++ server/config.py
@@ -4,4 +4,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///carebridge.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
-    DATABASE_URL=postgresql://dr:yourpassword@localhost:5432/database_name
+    DATABASE_URL = "postgresql://dr:yourpassword@localhost:5432/database_name"
